57da212855ee0feac9ad4d6dcd7027faa2d47b85
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-16 19:16:18 +0000

    gin and fiber handler

---
+++ internals/routes/SuperUserFiberRoutes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/lordofthemind/EventifyGo/internals/handlers"
+)
+
+func SetupRoutes(app *fiber.App, handler handlers.SuperUserFiberHandler) {
+	app.Post("/superusers", handler.CreateSuperUserHandler)
+	app.Get("/superusers/:id", handler.GetSuperUserByIDHandler)
+	app.Get("/superusers/email/:email", handler.GetSuperUserByEmailHandler)
+	app.Get("/superusers/username/:username", handler.GetSuperUserByUsernameHandler)
+	app.Post("/superusers/:id/enable2fa", handler.Enable2FAForSuperUserHandler)
+	app.Post("/superusers/:id/disable2fa", handler.Disable2FAForSuperUserHandler)
+	app.Get("/superusers/2fa", handler.GetAll2FAEnabledSuperUsersHandler)
+	app.Put("/superusers/:id/role", handler.UpdateSuperUserRoleHandler)
+	app.Put("/superusers/:id/permissions", handler.UpdateSuperUserPermissionsHandler)
+	app.Put("/superusers/:id/field/:field", handler.UpdateSuperUserFieldHandler)
+	app.Post("/superusers/:id/generate-reset-token", handler.GenerateAndSetResetTokenHandler)
+	app.Post("/superusers/:id/clear-reset-token", handler.ClearResetTokenHandler)
+	app.Delete("/superusers/:id", handler.DeleteSuperUserByIDHandler)
+	app.Get("/superusers/search", handler.SearchSuperUsersHandler)
+}
---
+++ internals/routes/SuperUserGinRoutes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lordofthemind/EventifyGo/internals/handlers"
+)
+
+func SetupSuperUserGinRoutes(r *gin.Engine, handler handlers.SuperUserGinHandler) {
+	r.POST("/superusers", handler.CreateSuperUserHandler)
+	r.GET("/superusers/:id", handler.GetSuperUserByIDHandler)
+	r.GET("/superusers/email/:email", handler.GetSuperUserByEmailHandler)
+	r.GET("/superusers/username/:username", handler.GetSuperUserByUsernameHandler)
+	r.POST("/superusers/:id/enable2fa", handler.Enable2FAForSuperUserHandler)
+	r.POST("/superusers/:id/disable2fa", handler.Disable2FAForSuperUserHandler)
+	r.GET("/superusers/2fa", handler.GetAll2FAEnabledSuperUsersHandler)
+	r.PUT("/superusers/:id/role", handler.UpdateSuperUserRoleHandler)
+	r.PUT("/superusers/:id/permissions", handler.UpdateSuperUserPermissionsHandler)
+	r.PUT("/superusers/:id/field/:field", handler.UpdateSuperUserFieldHandler)
+	r.POST("/superusers/:id/generate-reset-token", handler.GenerateAndSetResetTokenHandler)
+	r.POST("/superusers/:id/clear-reset-token", handler.ClearResetTokenHandler)
+	r.DELETE("/superusers/:id", handler.DeleteSuperUserByIDHandler)
+	r.GET("/superusers/search", handler.SearchSuperUsersHandler)
+}
